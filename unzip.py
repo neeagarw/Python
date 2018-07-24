f969d4dfacf7ecb74b2998e761d9f0cfaa14e6f3
neeagarw <41016085+neeagarw@users.noreply.github.com>
2018-07-24 10:03:17 -0500

    Update unzip.py

--- unzip.py
+++ unzip.py
@@ -1,4 +1,4 @@
 import zipfile  
   
-zip = zipfile.ZipFile(r'Python\Python.zip')  
-zip.extractall(r'\Python\output') 
+zip = zipfile.ZipFile(r'https://github.com/neeagarw/Python.git')  
+zip.extractall(r'https://github.com/neeagarw/Python.git') 
