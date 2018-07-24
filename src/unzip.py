f745e21f69606662c182d82428cdf4254892aef9
Neeraj Agarwala <neeraj.agarwala@nokia.com>
2018-07-24 09:48:53 -0500

    First commit

---
+++ src/unzip.py
@@ -0,0 +1,4 @@
+import zipfile  
+  
+zip = zipfile.ZipFile(r'C:\Users\agarwala\Desktop\Python\src')  
+zip.extractall(r'C:\Users\agarwala\Desktop\Python\src') 
---
+++ unzip.py
@@ -0,0 +1,4 @@
+import zipfile  
+  
+zip = zipfile.ZipFile(r'C:\Users\agarwala\Desktop\Python\Python.zip')  
+zip.extractall(r'C:\Users\agarwala\Desktop\Python\output') 
